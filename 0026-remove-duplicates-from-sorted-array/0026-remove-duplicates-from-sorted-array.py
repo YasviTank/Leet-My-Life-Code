aecddd4a6ff86eb8658c6a88be42ea73944f9802
Yasvi Tank <139741878+YasviTank@users.noreply.github.com>
2025-09-03 22:44:23 +0300

    Time: 3 ms (61.65%), Space: 14.1 MB (8.48%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.py
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.py
@@ -7,9 +7,9 @@ class Solution(object):
         n = len(nums)
         arr = []
         arr.append(nums[0])
-        # count = -1
+        count = -1
         for index in range(1, n):
-            if nums[index] != arr[-1]:
+            if nums[index] != arr[count]:
                 # print(index, nums[index], arr[count])
                 arr.append(nums[index])
                 # print(arr)
@@ -17,8 +17,7 @@ class Solution(object):
                 # count-=1
             else:
                 pass
-        m = len(arr)
-        for i in range(m):
+        for i in range(len(arr)):
             nums[i] = arr[i]
         return len(arr)
 
