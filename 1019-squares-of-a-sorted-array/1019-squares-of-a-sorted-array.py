3f568b546a352be48d5f1a260807c37a13939e35
Yasvi Tank <139741878+YasviTank@users.noreply.github.com>
2025-09-08 17:19:21 +0300

    Time: 15 ms (42.9%), Space: 14.4 MB (16.53%) - LeetHub

--- 1019-squares-of-a-sorted-array/1019-squares-of-a-sorted-array.py
+++ 1019-squares-of-a-sorted-array/1019-squares-of-a-sorted-array.py
@@ -4,8 +4,16 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[int]
         """
-        arr = []
-        for num in nums:
-            arr.append(abs(num)*abs(num))
-            print(num)
-        return sorted(arr)
+        res = [0] * len(nums)
+        left = 0
+        right = len(nums) - 1
+
+        for index in range(len(nums) -1, -1, -1):
+            if abs(nums[left]) > nums[right]:
+                res[index] = nums[left]*nums[left]
+                left += 1
+            else:
+                res[index] = nums[right]*nums[right]
+                right -= 1
+
+        return res
