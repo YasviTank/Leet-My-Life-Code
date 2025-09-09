b008f27f13cd0588bc980f017ab1a5580c266cea
Yasvi Tank <139741878+YasviTank@users.noreply.github.com>
2025-09-09 18:45:38 +0300

    Time: 3 ms (7.26%), Space: 12.3 MB (80.36%) - LeetHub

--- 0027-remove-element/0027-remove-element.py
+++ 0027-remove-element/0027-remove-element.py
@@ -5,15 +5,11 @@ class Solution(object):
         :type val: int
         :rtype: int
         """
-        count = 0
-        arr = []
-        for index in range(len(nums)):
-            if nums[index] != val:
-                arr.append(nums[index])
-                count+=1
-            else:
-                pass
-        for i in range(len(arr)):
-            nums[i] = arr[i]
-        return count
+        k = 0
+
+        for i in range(len(nums)):
+            if nums[i] != val:
+                nums[k] = nums[i]
+                k += 1
         
+        return k
