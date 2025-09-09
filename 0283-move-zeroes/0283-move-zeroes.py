a9e28d48b099dd1a157c3323e9454ae966cca673
Yasvi Tank <139741878+YasviTank@users.noreply.github.com>
2025-09-10 00:20:23 +0300

    Time: 6 ms (63.84%), Space: 13.5 MB (61.96%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.py
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -7,11 +7,12 @@ class Solution(object):
         n = len(nums)
         count = 0
         for index in range(n):
-            if nums[index] != 0 and n!=1:
-                nums[count] = nums[index]
+            if nums[index] != 0:
+                # nums[count] = nums[index]
+                nums[index], nums[count] = nums[count], nums[index]
                 # nums[index] = 0
                 count += 1
-        for i in range(count,n ):
-            if n!=1:
-                nums[i] = 0
+        # for i in range(count,n ):
+        #     if n!=1:
+        #         nums[i] = 0
             
