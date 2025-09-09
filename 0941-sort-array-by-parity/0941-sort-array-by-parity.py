392e0aebaa01672b129ae238a7442524acd746f0
Yasvi Tank <139741878+YasviTank@users.noreply.github.com>
2025-09-10 00:50:47 +0300

    Time: 0 ms (100%), Space: 13.1 MB (31.37%) - LeetHub

---
+++ 0941-sort-array-by-parity/0941-sort-array-by-parity.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def sortArrayByParity(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+
+        count = 0
+
+        for i in range(len(nums)):
+            if nums[i]%2 == 0:
+                nums[i], nums[count] = nums[count], nums[i]
+                count+=1
+
+        return nums
+
+        
