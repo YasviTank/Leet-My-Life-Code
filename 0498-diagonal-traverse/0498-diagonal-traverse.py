28e0512789f17fee53b1be00830aeaffd8f5a94c
Yasvi Tank <139741878+YasviTank@users.noreply.github.com>
2025-10-27 21:31:05 +0000

    Time: 5136 ms (5.11%), Space: 14.9 MB (31.21%) - LeetHub

--- 0498-diagonal-traverse/0498-diagonal-traverse.py
+++ 0498-diagonal-traverse/0498-diagonal-traverse.py
@@ -4,19 +4,47 @@ class Solution(object):
         :type mat: List[List[int]]
         :rtype: List[int]
         """
-        key = []
+        if not mat or not mat[0]:
+            return []
 
-        for i in range(len(mat)):
-            for j in range(len(mat[0])):
-                if i + j >= len(key):
-                    key.append([])
-                key[i + j].append(mat[i][j])
+        m = len(mat)
+        n = len(mat[0])
+        result = []
 
-        res = []
-        for i in range(len(key)):
-            if i % 2 == 0:
-                res.extend(reversed(key[i]))
-            else:
-                res.extend(key[i])
+        for summ in range(m + n - 1):
+            temp = []
 
-        return res
+            for row in range(m):
+                column = summ - row
+                # summ = row+column
+
+                if 0<=column<n:
+                    temp.append(mat[row][column])
+
+            if summ % 2 == 0:
+                temp.reverse()
+
+            result.extend(temp)
+
+        return result
+
+
+# ANOTHER SOLUTION:
+
+        # key = []
+
+        # for i in range(len(mat)):
+        #     for j in range(len(mat[0])):
+        #         if i + j >= len(key):
+        #             key.append([])
+        #         key[i + j].append(mat[i][j])
+
+        # res = []
+        # for i in range(len(key)):
+        #     if i % 2 == 0:
+        #         res.extend(reversed(key[i]))
+        #     else:
+        #         res.extend(key[i])
+
+        # return res
+        
