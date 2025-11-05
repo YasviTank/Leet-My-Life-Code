cff30acff8dff268a8c29188fba90b842ccfff10
Yasvi Tank <139741878+YasviTank@users.noreply.github.com>
2025-11-05 02:03:42 +0000

    Time: 1 ms (8.39%), Space: 8.8 MB (33.39%) - LeetHub

---
+++ 3515-find-if-digit-game-can-be-won/3515-find-if-digit-game-can-be-won.c
@@ -0,0 +1,17 @@
+bool canAliceWin(int* nums, int numsSize) {
+   int sum1 = 0, sum2 = 0;
+
+    for (int i = 0;i < numsSize;i++) {
+        if(nums[i] < 10) {
+            sum1 += nums[i];
+        } else {
+            sum2 += nums[i];
+        }
+    } 
+
+    if(sum1 != sum2) {
+        return true;
+    }
+
+    return false; 
+}
