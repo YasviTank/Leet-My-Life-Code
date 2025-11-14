e82a5605d969274c71edbab762f28db596d71f6b
Yasvi Tank <139741878+YasviTank@users.noreply.github.com>
2025-11-14 04:16:09 +0000

    Time: 0 ms (100%), Space: 13 MB (22.89%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.py
+++ 0035-search-insert-position/0035-search-insert-position.py
@@ -5,15 +5,32 @@ class Solution(object):
         :type target: int
         :rtype: int
         """
-        for i in range(len(nums)):
-            if nums[i] == target:
-                return i
+        # for i in range(len(nums)):
+        #     if nums[i] == target:
+        #         return i
 
-        for i in range(len(nums)):
-            if nums[i] > target:
-                print(i, nums[i])
-                return i 
-            if i == len(nums) - 1:
-                print(i)
-                return i + 1
-            
+        # for i in range(len(nums)):
+        #     if nums[i] > target:
+        #         print(i, nums[i])
+        #         return i 
+        #     if i == len(nums) - 1:
+        #         print(i)
+        #         return i + 1
+        # This solution is O(n)
+
+
+        start = 0
+        end = len(nums) -1
+        mid = 0
+
+        while(start <= end): 
+            mid = start + ( end - start)/2
+            if(nums[mid] == target): 
+                return mid
+            elif(nums[mid] < target): 
+                start = mid + 1
+            else:
+                end = mid - 1
+    
+        return start
+        # This solution is O(log n)
