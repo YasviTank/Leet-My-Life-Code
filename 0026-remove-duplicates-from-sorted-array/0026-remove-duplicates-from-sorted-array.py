49f91b01913f9784ea588cddbb6f3d7b433f2c21
Yasvi Tank <139741878+YasviTank@users.noreply.github.com>
2025-12-24 17:17:00 +0300

    Time: 0 ms (100%), Space: 18.4 MB (95.52%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.py
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.py
@@ -1,9 +1,5 @@
-class Solution(object):
-    def removeDuplicates(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: int
-        """
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
         i = 1
 
         for j in range(1, len(nums)):
@@ -12,3 +8,4 @@ class Solution(object):
                 i += 1
         
         return i
+
