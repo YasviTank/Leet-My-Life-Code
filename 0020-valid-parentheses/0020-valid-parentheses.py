3e9ab05452803e135fcea6a94624312cf1c8c4a4
Yasvi Tank <139741878+YasviTank@users.noreply.github.com>
2026-01-27 22:33:01 +0000

    Time: 5 ms (4.49%), Space: 19.5 MB (11.45%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.py
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -1,28 +1,44 @@
 class Solution:
     def isValid(self, s: str) -> bool:
         stack = []
+        mapping = {")":"(", "}":"{", "]":"["}
 
-        for element in s:
-            if element == "(" or element == "[" or element == "{":
-                stack.append(element)
-                print("appeneded")
-            else:
-                if len(stack) != 0:
-                    bracket = stack.pop()
-                    print(bracket)
-                    if element == ")" and bracket != "(":
-                        print("1")
-                        return False
-                    elif element == "}" and bracket != "{":
-                        return False
-                    elif element == "]" and bracket != "[":
-                        return False
-
-                else:
+        for char in s:
+            if char in mapping.values():
+                stack.append(char)
+            elif char in mapping.keys():
+                if not stack or mapping[char] != stack.pop():
                     return False
+        
+        return not stack
+
+
+
+# class Solution:
+#     def isValid(self, s: str) -> bool:
+#         stack = []
+
+#         for element in s:
+#             if element == "(" or element == "[" or element == "{":
+#                 stack.append(element)
+#                 print("appeneded")
+#             else:
+#                 if len(stack) != 0:
+#                     bracket = stack.pop()
+#                     print(bracket)
+#                     if element == ")" and bracket != "(":
+#                         print("1")
+#                         return False
+#                     elif element == "}" and bracket != "{":
+#                         return False
+#                     elif element == "]" and bracket != "[":
+#                         return False
+
+#                 else:
+#                     return False
        
-        if len(stack) != 0:
-            print("end false")
-            return False
-        else:
-            return True
+#         if len(stack) != 0:
+#             print("end false")
+#             return False
+#         else:
+#             return True
