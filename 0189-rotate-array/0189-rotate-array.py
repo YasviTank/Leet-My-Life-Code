fe37326fe8863dba1a784352cc52fa23987bee20
Yasvi Tank <139741878+YasviTank@users.noreply.github.com>
2026-03-04 10:30:17 +0000

    Time: 11 ms (27.59%), Space: 22.2 MB (99.83%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.py
+++ 0189-rotate-array/0189-rotate-array.py
@@ -17,13 +17,31 @@ class Solution(object):
         #     nums[i] = rotated[i]
 
 
-        # Get the actual number of rotations
-        k = k % len(nums)      
-        # Get the number of elements to move from the end to the beginning
-        r = len(nums) - k
-        # Store the elements to move
-        new = nums[0:r]
-        # Remove the elements from the beginning
-        nums[0:r] = []
-        # Append the stored elements to the end
-        nums.extend(new)
+        # # Get the actual number of rotations
+        # k = k % len(nums)      
+        # # Get the number of elements to move from the end to the beginning
+        # r = len(nums) - k
+        # # Store the elements to move
+        # new = nums[0:r]
+        # # Remove the elements from the beginning
+        # nums[0:r] = []
+        # # Append the stored elements to the end
+        # nums.extend(new)
+
+
+        n = len(nums)
+        k %= n  # In case k is larger than n
+        
+        # Helper function to reverse a portion of the array
+        def reverse(start, end):
+            while start < end:
+                nums[start], nums[end] = nums[end], nums[start]
+                start += 1
+                end -= 1
+        
+        # Reverse the entire array
+        reverse(0, n - 1)
+        # Reverse the first k elements
+        reverse(0, k - 1)
+        # Reverse the remaining n - k elements
+        reverse(k, n - 1)
