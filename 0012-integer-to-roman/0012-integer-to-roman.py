32df660b6307641ac51534b796ae49c5f5dc4354
Yasvi Tank <139741878+YasviTank@users.noreply.github.com>
2026-04-23 13:43:46 +0100

    Time: 6 ms (45.44%), Space: 19.3 MB (67.66%) - LeetHub

---
+++ 0012-integer-to-roman/0012-integer-to-roman.py
@@ -0,0 +1,35 @@
+class Solution:
+    def intToRoman(self, num: int) -> str:
+        hashmap = {1:"I", 5:"V", 10:"X", 50:"L", 100:"C", 500:"D", 1000:"M"}
+        unit = 1
+        s = ""
+
+        while num:
+            digit = (num % 10)
+            num = num // 10
+            if digit == 4 or digit == 9:
+                s1 = ""
+                if digit == 4:
+                    s1 += hashmap[5*unit]
+                else: 
+                    s1 += hashmap[10*unit]
+                print(s1)
+                s1 += hashmap[1*unit]
+                s += s1
+            else:
+                s1 = ""
+                if digit <= 3:
+                    for i in range(digit):
+                        s1 += hashmap[1 * unit]
+                else:
+                    digit = digit - 5
+                    
+                    for i in range(digit):
+                        s1 += hashmap[1 * unit]
+                    s1 += hashmap[5*unit]
+                s += s1
+            unit *= 10
+
+
+        return s[::-1]
+
