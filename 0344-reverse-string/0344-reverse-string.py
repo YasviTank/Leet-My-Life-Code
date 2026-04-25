0e75168be0a3285acf30cacda57c57a521e33d5b
Yasvi Tank <139741878+YasviTank@users.noreply.github.com>
2026-04-25 12:01:36 +0100

    Time: 0 ms (100%), Space: 23.3 MB (82.31%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.py
+++ 0344-reverse-string/0344-reverse-string.py
@@ -3,15 +3,26 @@ class Solution:
         """
         Do not return anything, modify s in-place instead.
         """
-        ## using recursion:
-        def reverse(left, right):
-            if left >= right:
-                return
+        l = 0
+        r = len(s) - 1
+        while l <= r:
+            temp = s[r]
+            s[r] = s[l]
+            s[l] = temp
+            # s[l], s[r] = s[r], s[l]
+            l += 1
+            r -= 1 
+        return s
+
+        # ## using recursion:
+        # def reverse(left, right):
+        #     if left >= right:
+        #         return
             
-            s[left], s[right]= s[right], s[left]
-            # s[right] = s[left]
+        #     s[left], s[right]= s[right], s[left]
+        #     # s[right] = s[left]
 
-            reverse(left + 1, right - 1)
+        #     reverse(left + 1, right - 1)
 
-        reverse(0, len(s) - 1)
+        # reverse(0, len(s) - 1)
         
